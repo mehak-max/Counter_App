7963e7eaeaca9346cc752e59bbcd4cf403036796
mehak-max <mehakaslam010@gmail.com>
2024-08-25 03:32:24 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter_App from './Components/Counter_App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div  >
+     <Counter_App/>  
+    </div>
+  );
+}
+
+export default App;
